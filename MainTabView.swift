a857c15b988bc5e7be69532ef9800256059f3ee5
Vrishti Kulkarni <136535843+vrishtik2007@users.noreply.github.com>
2024-10-02 17:33:21 -0400

    Add files via upload

---
+++ MainTabView.swift
@@ -0,0 +1,31 @@
+//
+
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    @StateObject var recipeData = RecipeData()
+
+    var body: some View {
+        TabView {
+            RecipeCategoryGridView()
+                .tabItem { Label("Recipes", systemImage: "list.dash") }
+            NavigationView {
+                RecipesListView(viewStyle: .favorites)
+            }.tabItem { Label("Favorites", systemImage: "heart.fill") }
+            SettingsView()
+                .tabItem { Label("Settings", systemImage: "gear") }
+        }
+        .environmentObject(recipeData)
+        .onAppear {
+            recipeData.loadRecipes()
+        }
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
